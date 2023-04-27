5d9a4517f1471c1cd27f9e3c7175f5ba4bedfb46
Oleksandr Lytvyn <tvinlee7@gmail.com>
2023-04-27 20:33:08 +0300

    start markup

---
+++ src/components/Button/Button.styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const ButtonCard = styled.button`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 14px 28px;
+  background: #5cd3a8;
+  box-shadow: 0px 3.43693px 3.43693px rgba(0, 0, 0, 0.25);
+  border-radius: 10.3108px;
+`;
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,11 @@
+import { Button } from 'components/Button/Button';
+import { CardMain } from './Card.styled';
+import { ButtonCard } from 'components/Button/Button.styled';
+
+export const Card = () => {
+  return (
+    <CardMain>
+      <ButtonCard>Following</ButtonCard>
+    </CardMain>
+  );
+};
