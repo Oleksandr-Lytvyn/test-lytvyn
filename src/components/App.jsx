327bdd8542b7dc0284184c2be18c0b8b70a8c1f7
Oleksandr Lytvyn <tvinlee7@gmail.com>
2023-05-01 20:45:14 +0300

    re-routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,6 @@
 import { Route, Routes } from 'react-router';
 import { Home } from 'pages/Home';
 import { Tweets } from 'pages/Tweets';
-import { NotFound } from 'pages/NotFound';
 
 export const App = () => {
   return (
@@ -9,7 +8,7 @@ export const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/tweets" element={<Tweets />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="*" element={<Home />} />
       </Routes>
     </div>
   );
--- src/pages/NotFound.jsx
+++
@@ -1,7 +0,0 @@
-export const NotFound = () => {
-  return (
-    <div className="box">
-      <h1>page not found</h1>
-    </div>
-  );
-};
