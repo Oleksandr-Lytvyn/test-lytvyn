c838d47694c9a02996082e9e5487ed7ead58f61b
Oleksandr Lytvyn <tvinlee7@gmail.com>
2023-05-01 14:54:03 +0300

    add TweetsList.jsx

---
+++ src/components/TweetsList/TweetsList.jsx
@@ -0,0 +1,49 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+import { Card } from '../Card/Card';
+
+let page = 1;
+
+export const TweetsList = () => {
+  const [users, setUsers] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const { data } = await axios.get(
+        `https://63bf12e0e348cb07622066df.mockapi.io/users?page=${page}&limit=3`
+      );
+      page += 1;
+      setUsers(data);
+    } catch (error) {
+      return error;
+    }
+  };
+
+  const nextPage = async () => {
+    try {
+      const { data } = await axios.get(
+        `https://63bf12e0e348cb07622066df.mockapi.io/users?page=${page}&limit=3`
+      );
+      page += 1;
+      setUsers([...users, ...data]);
+    } catch (error) {
+      return error;
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      {users.map(user => {
+        return <Card user={user} key={user.id}></Card>;
+      })}
+      <button type="button" onClick={nextPage}>
+        load more
+      </button>
+    </>
+  );
+};
